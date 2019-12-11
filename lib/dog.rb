65a9f46d5ce8b912dc2d7bb889235ad83cdf6a86
Chris Cheong <chrisxcheong@gmail.com>
2019-12-11 00:29:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,13 @@ class Dog
     @breed = breed
   end
 
+  def create_table
+    sql = <<- SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    )
+    SQL
+  end
 end
