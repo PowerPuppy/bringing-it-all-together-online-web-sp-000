43dab96eec25c2fe124e529e896149161e80ef16
Chris Cheong <chrisxcheong@gmail.com>
2019-12-11 01:33:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,5 +52,11 @@ class Dog
     dog
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE id = ?
+    SQL
+    DB[:conn].execute(sql,id).map {|row| Dog.new_from_db(row)first}.first
+  end
 
 end
