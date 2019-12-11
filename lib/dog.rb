84b896f366129971538aab10cb14223555a75f2d
Chris Cheong <chrisxcheong@gmail.com>
2019-12-11 01:24:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,7 @@
 class Dog
+  attr_accessor :id, :name, :breed
 
-  attr_accessor :name, :breed
-  attr_reader :id
-
-  def initialize(name:, breed:, id: nil)
+  def initialize(id: nil, name: nil, breed: nil)
     @id = id
     @name = name
     @breed = breed
@@ -11,10 +9,10 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      breed TEXT
+    CREATE TABLE IF NOT EXISTS dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
     )
     SQL
 
@@ -22,15 +20,12 @@ class Dog
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS dogs"
+    sql = <<-SQL
+    DROP TABLE IF EXISTS dogs
+    SQL
+
     DB[:conn].execute(sql)
   end
-
-  def update
-    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
-  end
-
 
   def save
     if self.id
@@ -46,40 +41,4 @@ class Dog
     end
     self
   end
-
-  def self.create(name:, breed:)
-    dog=  self.new(name: name, breed: breed)
-    dog.save
-  end
-
-  def self.new_from_db(row)
-    dog = self.new(name: row[1],breed: row[2],id: row[0])
-    dog
-  end
-
-  def self.find_by_id(id)
-    sql = <<-SQL
-      SELECT * FROM dogs WHERE id = ?
-      SQL
-      DB[:conn].execute(sql,id).map { |row| Dog.new_from_db(row)}.first
-  end
-
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT * FROM dogs WHERE name = ?
-      SQL
-      DB[:conn].execute(sql,name).map { |row| Dog.new_from_db(row)}.first
-  end
-
-  def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    if !dog.empty?
-      dog_data = dog[0]
-      dog = self.new(id: dog_data[0],name: dog_data[1],breed: dog_data[2])
-    else
-      dog = self.create(name: name, breed: breed)
-    end
-    dog
-  end
-
 end
